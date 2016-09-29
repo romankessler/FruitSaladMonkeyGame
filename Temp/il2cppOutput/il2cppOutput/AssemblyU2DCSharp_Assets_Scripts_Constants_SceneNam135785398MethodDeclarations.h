fd316c9637f63806e7af0eb39e89aac7c2846e98
romankessler <Roman@Romans-MacBook-Pro.local>
2016-09-29 20:32:08 +0200

    Ground Platform prefab added

---
+++ Temp/il2cppOutput/il2cppOutput/AssemblyU2DCSharp_Assets_Scripts_Constants_SceneNam135785398MethodDeclarations.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "il2cpp-config.h"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+#include <stdint.h>
+#include <assert.h>
+#include <exception>
+
+// Assets.Scripts.Constants.SceneNames
+struct SceneNames_t135785398;
+
+#include "codegen/il2cpp-codegen.h"
+
+// System.Void Assets.Scripts.Constants.SceneNames::.ctor()
+extern "C"  void SceneNames__ctor_m2501218527 (SceneNames_t135785398 * __this, const MethodInfo* method) IL2CPP_METHOD_ATTR;
---
+++ Temp/il2cppOutput/il2cppOutput/AssemblyU2DCSharp_StartSound409099757MethodDeclarations.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "il2cpp-config.h"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+#include <stdint.h>
+#include <assert.h>
+#include <exception>
+
+// StartSound
+struct StartSound_t409099757;
+// UnityEngine.Animator
+struct Animator_t2776330603;
+
+#include "codegen/il2cpp-codegen.h"
+#include "UnityEngine_UnityEngine_Animator2776330603.h"
+#include "UnityEngine_UnityEngine_AnimatorStateInfo323110318.h"
+
+// System.Void StartSound::.ctor()
+extern "C"  void StartSound__ctor_m2814350670 (StartSound_t409099757 * __this, const MethodInfo* method) IL2CPP_METHOD_ATTR;
+// System.Void StartSound::OnStateEnter(UnityEngine.Animator,UnityEngine.AnimatorStateInfo,System.Int32)
+extern "C"  void StartSound_OnStateEnter_m3768970606 (StartSound_t409099757 * __this, Animator_t2776330603 * ___animator0, AnimatorStateInfo_t323110318  ___stateInfo1, int32_t ___layerIndex2, const MethodInfo* method) IL2CPP_METHOD_ATTR;

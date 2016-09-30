2903a5be43d722086e675cb31a692ca5824fe9eb
romankessler <Roman@Romans-MacBook-Pro.local>
2016-10-01 00:12:25 +0200

    camera follow added

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -12,18 +12,38 @@ public class CameraFollow : MonoBehaviour {
 	[SerializeField]
 	private float yMax;
 
+	private float _lastYVelocity;
+
 	[SerializeField]
 	private GameObject _cameraTarget;
 
 	private Transform _target;
 
+	private Rigidbody2D _targetRigidBody;
+
 	// Use this for initialization
 	void Start () {
 		_target = _cameraTarget.transform;
+		_targetRigidBody = _cameraTarget.GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
 	void LateUpdate () {
-		transform.position = new Vector3 (Mathf.Clamp(_target.position.x, _xMin, _xMax), Mathf.Clamp(_target.position.y, _yMin, yMax), transform.position.z); 
+
+		var yVelocity = _targetRigidBody.velocity.y;
+
+		if (yVelocity < 10 && yVelocity > -0.1) {
+
+			yVelocity = _lastYVelocity - (_lastYVelocity / 10);
+		} 
+
+		_lastYVelocity = yVelocity;
+
+		var targetYPosition = _target.position.y + (yVelocity < 0 ? yVelocity / 10 : yVelocity / 20);
+
+		transform.position = new Vector3 (
+			Mathf.Clamp(_target.position.x, _xMin, _xMax), 
+			Mathf.Clamp(targetYPosition, _yMin, yMax), 
+			transform.position.z); 
 	}
 }

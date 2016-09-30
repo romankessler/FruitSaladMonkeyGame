1031f0e6544016b2f1cd8dd267f313288d11a607
romankessler <Roman@Romans-MacBook-Pro.local>
2016-09-30 23:24:50 +0200

    camera follow fix

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	[SerializeField]
+	private float _xMin;
+	[SerializeField]
+	private float _xMax;
+	[SerializeField]
+	private float _yMin;
+	[SerializeField]
+	private float yMax;
+
+	[SerializeField]
+	private GameObject _cameraTarget;
+
+	private Transform _target;
+
+	// Use this for initialization
+	void Start () {
+		_target = _cameraTarget.transform;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = new Vector3 (Mathf.Clamp(_target.position.x, _xMin, _xMax), Mathf.Clamp(_target.position.y, _yMin, yMax), transform.position.z); 
+	}
+}

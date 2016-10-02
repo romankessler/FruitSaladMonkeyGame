0eada827414d7011ad5283a27b7931131daf0b00
romankessler <Roman@Romans-MacBook-Pro.local>
2016-10-02 10:29:09 +0200

    camera area added

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,17 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(Camera))]
 public class CameraFollow : MonoBehaviour
 {
 
-	[SerializeField]
+
 	private float _xMin;
-	[SerializeField]
+
 	private float _xMax;
-	[SerializeField]
+
 	private float _yMin;
-	[SerializeField]
-	private float yMax;
+
+	private float _yMax;
 
 	private float _lastYVelocity;
 
@@ -24,11 +25,50 @@ public class CameraFollow : MonoBehaviour
 
 	private Rigidbody2D _targetRigidBody;
 
+	private BoxCollider2D _cameraArea;
+
+	private GameObject _cameraAreaObject;
+
+	private Camera _camera;
+
 	// Use this for initialization
 	void Start ()
 	{
 		_target = _cameraTarget.transform;
 		_targetRigidBody = _cameraTarget.GetComponent<Rigidbody2D> ();
+
+		_cameraAreaObject = GameObject.Find ("CameraArea");
+		_cameraArea = _cameraAreaObject.GetComponent<BoxCollider2D> ();
+
+		_camera = _cameraAreaObject.GetComponentInChildren<Camera> ();
+
+		var cameraHeight = 2f * _camera.orthographicSize;
+		Debug.Log ("cameraHeight="+cameraHeight);
+		var cameraWidth = cameraHeight * _camera.aspect;
+		Debug.Log ("cameraWidth="+cameraWidth);
+
+		var areaXPos = _cameraAreaObject.transform.position.x;
+		var areaYPos = _cameraAreaObject.transform.position.y;
+
+		Debug.Log ("areaXPos="+areaXPos);
+		Debug.Log ("areaYPos="+areaYPos);
+
+		var areaWidth = _cameraArea.size.x;
+		var areaHeight = _cameraArea.size.y;
+
+		Debug.Log ("areaWidth="+areaWidth);
+		Debug.Log ("areaHeight="+areaHeight);
+
+		_xMin = areaXPos - areaWidth/2 + cameraWidth/2;// - _cameraAerea.offset.x;
+		_xMax = _xMin + areaWidth - cameraWidth;
+
+		_yMin = areaYPos - areaHeight/2 + cameraHeight/2;//; - _cameraAerea.offset.y;
+		_yMax = _yMin + areaHeight - cameraHeight;
+
+		Debug.Log ("xMin=" + _xMin);
+		Debug.Log ("xMax=" + _xMax);
+		Debug.Log ("yMin=" + _yMin);
+		Debug.Log ("yMax=" + _yMax);
 	}
 	
 
@@ -42,7 +82,7 @@ public class CameraFollow : MonoBehaviour
 
 		transform.position = new Vector3 (
 			Mathf.Clamp (_targetRigidBody.position.x, _xMin, _xMax), 
-			Mathf.Clamp (targetYPosition, _yMin, yMax), 
+			Mathf.Clamp (targetYPosition, _yMin, _yMax), 
 			transform.position.z); 
 	}
 

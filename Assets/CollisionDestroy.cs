8497301f4af9086c16626abe7ac7fff5aee7bbd3
romankessler <Roman@Romans-MacBook-Pro.local>
2016-10-02 19:33:56 +0200

    enemy added

---
+++ Assets/CollisionDestroy.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts.Constants;
+
+public class CollisionDestroy : MonoBehaviour {
+
+	[SerializeField]
+	private BoxCollider2D _triggerArea;
+
+	[SerializeField]
+	private  AudioClip _triggerSoundEffect;
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.CompareTag(TagNames.PLAYER) && _triggerArea.IsTouching(other))
+		{
+			AudioSource.PlayClipAtPoint(_triggerSoundEffect, other.transform.position);
+			Destroy(gameObject);
+		}
+	}
+
+	void OnTriggerStay2D(Collider2D other)
+	{
+		if (other.CompareTag(TagNames.PLAYER) && _triggerArea.IsTouching(other))
+		{
+			AudioSource.PlayClipAtPoint(_triggerSoundEffect, other.transform.position);
+			Destroy(gameObject);
+		}
+	}
+}

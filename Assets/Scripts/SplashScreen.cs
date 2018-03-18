c5cde0b7f47e198022b991cef68b420498d32961
Funkyhead <roman-kessler@bluewin.ch>
2018-03-18 21:07:19 +0100

    Splashscreen

--- Assets/Scripts/SplashScreen.cs
+++ Assets/Scripts/SplashScreen.cs
@@ -1,54 +1,63 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
+using System;
 using Holoville.HOTween;
-using UnityEngine;
-using UnityEngine.SceneManagement;
 
 /// <summary>
-///     This class is used to animate the splatch screen
-///     Author : Pondomaniac Games
+///  This class is used to animate the splatch screen 
+/// Author : Pondomaniac Games
 /// </summary>
-public class SplashScreen : MonoBehaviour
+public class SplatchScreen : MonoBehaviour
 {
-    public float _FadeInTime; //The fadeIn animation time
-    public float _FadeOutTime; //The fadeOut animation time
-    public GameObject _Logo; //The logo to animate
-    public float _WaitingTime; //The stay time of the logo
+		public GameObject _Logo;//The logo to animate
+		public float _FadeInTime;//The fadeIn animation time
+		public float _WaitingTime;//The stay time of the logo
+		public float _FadeOutTime;//The fadeOut animation time
+		public string _nextScene;//The next scene to load
 
-    // Used before initialization
-    private void Awake()
-    {
-        Time.timeScale = 1;
-    }
+		// Used before initialization
+		void Awake ()
+		{
+				Time.timeScale = 1; 
+		}
+		                                                                                        
+		// Used for initialization
+		void Start ()
+		{
+				StartCoroutine (Init ());
+		}
 
-    // Used for initialization
-    private void Start()
-    {
-        StartCoroutine(Init());
-    }
+	    // Animate the Logos with fadeIn and fadeOut effect
+		IEnumerator Init ()
+		{    	
+				Sequence mySequence = new Sequence (new SequenceParms ());
+				TweenParms parms;
+					
+				Color oldColor = _Logo.GetComponent<Renderer>().material.color;
 
-    // Animate the Logos with fadeIn and fadeOut effect
-    private IEnumerator Init()
-    {
-        var mySequence = new Sequence(new SequenceParms());
-        TweenParms parms;
+				_Logo.GetComponent<Renderer>().material.color = new Color (oldColor.r, oldColor.b, oldColor.g, 0f);
 
-        var oldColor = _Logo.GetComponent<Renderer>().material.color;
+				parms = new TweenParms ().Prop ("color", new Color (oldColor.r, oldColor.b, oldColor.g, 1f)).Ease (EaseType.EaseInQuart);
 
-        _Logo.GetComponent<Renderer>().material.color = new Color(oldColor.r, oldColor.b, oldColor.g, 0f);
+				mySequence.Append (HOTween.To (_Logo.GetComponent<Renderer>().material, _FadeInTime, parms));
+				mySequence.Append (HOTween.To (_Logo.GetComponent<Renderer>().material, _WaitingTime, parms));
 
-        parms = new TweenParms().Prop("color", new Color(oldColor.r, oldColor.b, oldColor.g, 1f))
-            .Ease(EaseType.EaseInQuart);
+				parms = new TweenParms ().Prop ("color", new Color (oldColor.r, oldColor.b, oldColor.g, 0f));
 
-        mySequence.Append(HOTween.To(_Logo.GetComponent<Renderer>().material, _FadeInTime, parms));
-        mySequence.Append(HOTween.To(_Logo.GetComponent<Renderer>().material, _WaitingTime, parms));
+				mySequence.Append (HOTween.To (_Logo.GetComponent<Renderer>().material, _FadeOutTime, parms));
 
-        parms = new TweenParms().Prop("color", new Color(oldColor.r, oldColor.b, oldColor.g, 0f));
+				mySequence.Play ();
+		
+				yield return new WaitForSeconds (_FadeInTime + _WaitingTime + _FadeOutTime);
+				Application.LoadLevel (_nextScene); 
+		
+		
+		}
 
-        mySequence.Append(HOTween.To(_Logo.GetComponent<Renderer>().material, _FadeOutTime, parms));
 
-        mySequence.Play();
 
-        yield return new WaitForSeconds(_FadeInTime + _WaitingTime + _FadeOutTime);
-        Application.LoadLevel(SceneManager.GetActiveScene().buildIndex + 1);
-    }
+
+	
+
+
 }

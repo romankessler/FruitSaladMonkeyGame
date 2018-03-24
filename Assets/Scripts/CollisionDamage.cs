39ecf34381959c16daab28e74de1bdfeb60ad1c3
Funkyhead <roman-kessler@bluewin.ch>
2018-03-24 15:40:17 +0100

    force on damage

--- Assets/Scripts/CollisionDamage.cs
+++ Assets/Scripts/CollisionDamage.cs
@@ -1,24 +1,60 @@
-﻿using Assets.Scripts.Constants;
-
+﻿using System;
+using Assets.Scripts.Constants;
 using UnityEngine;
 
 public class CollisionDamage : MonoBehaviour
 {
-    public float _damage = 1;
+    public enum ForceDirection
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
 
-    void OnTriggerEnter2D(Collider2D other)
+    private const int MULTIPLIER = 100;
+    public float _damage = 1;
+    public int ForcePower = 0;
+    public ForceDirection ForeDirection;
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag(TagNames.PLAYER))
         {
             other.SendMessage("ApplyDamage", _damage);
+            AddForce(other);
         }
     }
 
-    void OnTriggerStay2D(Collider2D other)
+    private void AddForce(Collider2D other)
+    {
+        var force = new Vector2();
+        switch (ForeDirection)
+        {
+            case ForceDirection.Up:
+                force.y = ForcePower * MULTIPLIER;
+                break;
+            case ForceDirection.Down:
+                force.y = ForcePower * MULTIPLIER * -1;
+                break;
+            case ForceDirection.Left:
+                force.x = ForcePower * MULTIPLIER * -1;
+                break;
+            case ForceDirection.Right:
+                force.x = ForcePower * MULTIPLIER;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+        other.attachedRigidbody.AddForce(force);
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
     {
         if (other.CompareTag(TagNames.PLAYER))
         {
             other.SendMessage("ApplyDamage", _damage);
+            AddForce(other);
         }
     }
 }

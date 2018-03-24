08e3782f6ce26a666a03239f884f713660188f23
Funkyhead <roman-kessler@bluewin.ch>
2018-03-24 15:39:47 +0100

    renaming screenrotationcontroller

---
+++ Assets/ScreenRotationController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class ScreenRotationController : MonoBehaviour
+{
+    // Use this for initialization
+    private void Start()
+    {
+        Screen.autorotateToLandscapeRight = false;
+        Screen.orientation = ScreenOrientation.LandscapeRight;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
